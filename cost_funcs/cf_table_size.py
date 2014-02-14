e929779d3809693c7f25a7ba7560ff95863e3200
katrina <user@server>
2014-02-14 09:01:11 -0500

    Made cf_tablesize quartic

--- cost_funcs/cf_table_size.py
+++ cost_funcs/cf_table_size.py
@@ -5,7 +5,7 @@ def cf_table_size(tables):
     for table in tables:
         if table.name != 'Head':
             distance_from_capacity = len(table.people) - int(table.capacity)
-            cost += abs(distance_from_capacity)
+            cost += abs(distance_from_capacity)**4
             maximum = max(maximum, abs(distance_from_capacity))
 
     print "Max diff btwn table size and optimum size: " + str(maximum)
