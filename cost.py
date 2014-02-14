3da48a6ec80c00e2ebaeb52ac4d1a8f87989e7e9
katrina <user@server>
2014-02-14 09:30:12 -0500

    Improved cf_overlaps

--- cost.py
+++ cost.py
@@ -1,7 +1,7 @@
 from itertools import chain, combinations
 from cost_funcs.cf_category_balance import cf_category_balance
 #from cost.cf_new_connections import cf_newconnections
-#from cost.cf_overlaps import cf_overlaps
+from cost_funcs.cf_overlaps import cf_overlaps
 from cost_funcs.cf_same_spot import cf_same_spot
 from cost_funcs.cf_table_size import cf_table_size
 
@@ -11,5 +11,6 @@ def cost_of(solution):
     weights = [1, 1, 1, 1, 1, 1, 1, 1, 1]
     total_cost = weights[0] * cf_same_spot(tables_out) + \
                  weights[4] * cf_table_size(tables_out) + \
-                 weights[5] * cf_category_balance(tables_out)
+                 weights[5] * cf_category_balance(tables_out) +\
+                 weights[1] * cf_overlaps(tables_out)
     return total_cost
